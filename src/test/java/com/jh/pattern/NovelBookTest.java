4ca9c3de59b2b844ab492f80ca0e80ad29e0ed0c
jianghua2 <jianghua2@100tal.com>
2018-06-14 21:45:31 +0800

    b

---
+++ src/test/java/com/jh/pattern/NovelBookTest.java
@@ -0,0 +1,12 @@
+package com.jh.pattern;
+
+import junit.framework.TestCase;
+
+public class NovelBookTest extends TestCase {
+    String a = "a";
+    String b = "a";
+    public void testa(){
+        super.assertEquals(a,b);
+    }
+
+}

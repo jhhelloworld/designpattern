c5638c65025ec2f715802a53cbbde35ffa73bf9c
jhhelloworld <jh_helloworld@163.com>
2018-06-20 22:53:21 +0800

    a

---
+++ src/main/java/com/jh/pattern/singleton/Emperor.java
@@ -0,0 +1,8 @@
+package com.jh.pattern.singleton;
+
+/**
+ * jh
+ * 2018年06月18日  19：01
+ */
+public class Emperor {
+}
---
+++ src/main/java/com/jh/pattern/singleton/Minister.java
@@ -0,0 +1,8 @@
+package com.jh.pattern.singleton;
+
+/**
+ * jh
+ * 2018年06月18日  19：00
+ */
+public class Minister {
+}

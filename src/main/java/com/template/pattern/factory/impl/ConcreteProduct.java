b15ffd84e013127160d74dfe89ad0401ddb35933
jianghua2 <jianghua2@100tal.com>
2018-06-21 22:09:57 +0800

    a

---
+++ src/main/java/com/template/pattern/factory/impl/ConcreteProduct.java
@@ -0,0 +1,14 @@
+package com.template.pattern.factory.impl;
+
+import com.template.pattern.factory.inter.Product;
+
+/**
+ * id001
+ * 2018年06月21日  21：58
+ */
+public class ConcreteProduct extends Product {
+    @Override
+    public void method2() {
+
+    }
+}
---
+++ src/main/java/com/template/pattern/factory/inter/Product.java
@@ -0,0 +1,15 @@
+package com.template.pattern.factory.inter;
+
+/**
+ * id001
+ * 2018年06月21日  21：53
+ * 抽象产品类
+ */
+public abstract class Product {
+    public void method1(){
+
+    }
+
+    public abstract void method2();
+
+}
